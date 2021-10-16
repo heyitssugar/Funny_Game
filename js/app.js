51e53288ecceb2df9378fc765afd9b008f4e26a3
Sugar <heyitsssugar@outlook.com>
2021-10-16 02:54:32 +0200

    Upload The Design

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const gig = document.getElementById('flag');
+gig.addEventListener('click', ()=> {
+  window.alert("Congratulations!\nyou caught me!\nyou are really AWESOME Take this screenshot and send it to us on our Discord Server!");
+});
